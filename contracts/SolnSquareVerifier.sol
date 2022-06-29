f3262e53a1c3e97077c1560637591b847e806be4
WooKaiZen <102263991+WooKaiZen@users.noreply.github.com>
2022-06-30 01:58:20 +0200

    Add files via upload

--- contracts/SolnSquareVerifier.sol
+++ contracts/SolnSquareVerifier.sol
@@ -1,44 +1,33 @@
 pragma solidity >=0.4.21 <0.6.0;
 import './ERC721Mintable.sol';
 //import './Verifier.sol';
-
-
-
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is ERC721Mintable {
-
     Verifier verifier;
 // TODO define a solutions struct that can hold an index & an address
     struct Solution {
         uint256 tokenId;
         address to;
     }
-
 // TODO define an array of the above struct
     Solution[] solutions;
-
 // TODO define a mapping to store unique solutions submitted
-    mapping(bytes32 => Solution) uniqueSolutions;
-
-
+    mapping(bytes32 => Solution) public uniqueSolutions;
+    mapping(bytes32 => bool) solutionExists;
 // TODO Create an event to emit when a solution is added
     event SolutionAdded(address from, uint256 tokenId);//TODO:check
-
-    constructor(address VerifierAddress) ERC721Mintable("RealEstateToken","RET") public {
-        verifier = Verifier(VerifierAddress);
+    constructor(address verifierAddress, string memory name, string memory symbol) ERC721Mintable(name,symbol) public {
+        verifier = Verifier(verifierAddress);
     }
-
 // TODO Create a function to add the solutions to the array and emit the event
     function addSolution(uint256 tokenId, address to, bytes32 key) internal {
         Solution memory solution = Solution(tokenId,to);
         uniqueSolutions[key] = solution;
+        solutionExists[key] = true;
         solutions.push(solution);
         emit SolutionAdded(msg.sender,solutions.length);
     }
-
-
 // TODO Create a function to mint new NFT only after the solution has been verified
-
 //  - make sure you handle metadata as well as tokenSupply
     function mintToken (
         uint256 tokenId,
@@ -46,7 +35,7 @@ contract SolnSquareVerifier is ERC721Mintable {
         uint[2] memory a,
         uint[2][2] memory b,
         uint[2] memory c,
-        uint[2] memory input
+        uint[1] memory input
     ) public {
         //  - make sure the solution is unique (has not been used before)
         bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
@@ -55,38 +44,31 @@ contract SolnSquareVerifier is ERC721Mintable {
         addSolution(tokenId,to,key);
         super.mint(to,tokenId);
     }
+
+    function getSolutionAddress(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[1] memory input
+    ) public view returns(address) {
+        bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
+        return uniqueSolutions[key].to;
+    }
+
+    function doesSolutionExist(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[1] memory input
+    ) public view returns(bool) {
+        bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
+        if (solutionExists[key]!=true) {
+            return false;
+        }
+        return true;
+    }
 }
 
-
-// TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 contract Verifier {
-    function verifyTx(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory inputs) public view returns (bool);
+    function verifyTx(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[1] memory inputs) public view returns (bool);
 }
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- contracts/Verifier.sol
+++ contracts/Verifier.sol
@@ -61,7 +61,7 @@ library Pairing {
         input[3] = p2.Y;
         bool success;
         assembly {
-            success := staticcall(sub(gas(), 2000), 6, input, 0xc0, r, 0x60)
+            success :=staticcall(sub(gas(), 2000), 6, input, 0xc0, r, 0x60)
             // Use "invalid" to make gas estimation work     
             switch success case 0 { invalid() }
         }
@@ -173,6 +173,7 @@ contract Verifier {
         Pairing.G2Point b;
         Pairing.G1Point c;
     }
+
     function verifyingKey() pure internal returns (VerifyingKey memory vk) {
         vk.alpha = Pairing.G1Point(uint256(0x0688bd5f7dcbaa8804ddf3070af49b8dec6c80d812d6698c29e66e1acb5b863d), 
                     uint256(0x16ee71919fa91502d13e54efab97ebd09cdff3fb6f97abc18eae8838efe8865d));
@@ -197,7 +198,7 @@ contract Verifier {
     function verify(uint[] memory input, Proof memory proof) internal view returns (uint) {
         uint256 snark_scalar_field = 21888242871839275222246405745257275088548364400416034343698204186575808495617;       
         VerifyingKey memory vk = verifyingKey();
-        require(input.length + 1 == vk.gamma_abc.length);    
+        require(input.length+1== vk.gamma_abc.length);  
         // Compute the linear combination vk_x
         Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0); 
         for (uint i = 0; i < input.length; i++) {
@@ -209,18 +210,19 @@ contract Verifier {
              proof.a, proof.b,
              Pairing.negate(vk_x), vk.gamma,
              Pairing.negate(proof.c), vk.delta,
-             Pairing.negate(vk.alpha), vk.beta)) return 1;   
+             Pairing.negate(vk.alpha), vk.beta)) return 1;
         return 0;
     }
     function verifyTx(
-            uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[2] memory input
+            uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[1] memory input
         ) public view returns (bool r) {
+        
         Proof memory proof;
         proof.a = Pairing.G1Point(a[0],a[1]);
         proof.b = Pairing.G2Point([b[0][0],b[0][1]],[b[1][0],b[1][1]]);
         proof.c = Pairing.G1Point(c[0],c[1]);
 
-        uint[] memory inputValues = new uint[](1);
+        uint[] memory inputValues = new uint[](input.length);
 
         for(uint i = 0; i < input.length; i++){
             inputValues[i] = input[i];
@@ -230,5 +232,7 @@ contract Verifier {
         } else {
             return false;
         }
+        
+        return false;
     }
 }
