7b1afb102e1a4f4c704ef9b72390ada85b7732c7
WooKaiZen <102263991+WooKaiZen@users.noreply.github.com>
2022-06-30 02:05:16 +0200

    Add files via upload

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+require("dotenv").config();
+const fs = require('fs');
 
 module.exports = {
   /**
@@ -68,6 +66,15 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
+
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(process.env.MNEMONIC, `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`);
+      },
+      network_id: 4,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
 
     // Useful for private networks
     // private: {
